9ab9554ec083ffb67c36036566196af47e64194c
TURKCELL\USERNAME <email@turkcell.com.tr>
2021-06-24 23:11:39 +0300

    Init project

---
+++ Bortom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bortom
+//
+//  Created by Dmitry Semenuk on 24/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
